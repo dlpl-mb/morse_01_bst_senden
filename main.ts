c73bdc8a141e1c8eb35523316b7820486dda99f8
Denken lernen - Probleme lösen <83607557+dlpl-mb@users.noreply.github.com>
2021-09-18 00:00:38 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,79 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    zufall = randint(0, 25)
+    basic.showString("" + (liste_buchstaben[zufall]))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("" + (liste_morsecodes[zufall]))
+})
+let zufall = 0
+let liste_morsecodes: string[] = []
+let liste_buchstaben: string[] = []
+basic.showIcon(IconNames.Yes)
+liste_buchstaben = [
+"A",
+"B",
+"C",
+"D",
+"E",
+"F",
+"G",
+"H",
+"I",
+"J",
+"K",
+"L",
+"M",
+"N",
+"O",
+"P",
+"Q",
+"R",
+"S",
+"T",
+"U",
+"V",
+"W",
+"X",
+"Y",
+"Z"
+]
+liste_morsecodes = [
+".-",
+"-...",
+"-.-.",
+"-..",
+".",
+"..-.",
+"--.",
+"....",
+"..",
+".---",
+"-.-",
+".-..",
+"--",
+"-.",
+"---",
+".--.",
+"--.-",
+".-.",
+"...",
+"-",
+"..-",
+"...-",
+".--",
+"-..-",
+"-.--",
+"--.."
+]
